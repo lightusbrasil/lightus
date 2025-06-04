328f97c8dea6a622ac45ef54e873a2e99d496e28
lightusbrasil <lightusct@gmail.com>
2025-06-04 12:12:35 -0300

    Add files via upload

--- js/register-head.js
+++ js/register-head.js
@@ -45,7 +45,7 @@
     // Assets globais que sempre serão carregados
     const globalAssets = [
         'components/common.css',
-        'https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap',
+        'https://fonts.googleapis.com/css2?family=Manrope:wght@100;200;300;400;500;600;700;800;900&display=swap',
         'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
         'favicon.ico'
     ];
